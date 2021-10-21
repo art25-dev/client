ab05585db90bcc3f391b6cc8fc587de78be7bf92
art25-dev <65493127+art25-dev@users.noreply.github.com>
2021-10-21 17:04:38 +0300

    add socket

--- src/main.js
+++ src/main.js
@@ -2,13 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-3-socket.io'
+import SocketIO from 'socket.io-client'
 import "@/assets/styles/fonts.scss";
 import moment from 'moment'
 moment.locale('ru');  
 
 import Icon from '@/components/Icon';
 
+
+
 const app = createApp(App)
+
+app.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+  }
+}))
 
 app.config.globalProperties.$moment = moment
 app.component('icon', Icon)
